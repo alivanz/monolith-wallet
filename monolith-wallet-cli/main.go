dfccbcdc0da9e937f32f5d16b897d3a0b712d7f6
alivan <alivan1627@gmail.com>
2018-10-31 02:42:27 +0700

    base

---
+++ monolith-wallet-cli/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strconv"
+	"strings"
+
+	crypto "github.com/alivanz/go-crypto"
+	microwallet "github.com/alivanz/go-microwallet"
+)
+
+const (
+	// Coin
+	Bitcoin  = "btc"
+	Dogecoin = "doge"
+	Ethereum = "eth"
+
+	// Action
+	GetAddress = "get-address"
+	GetBalance = "get-balance"
+	Transfer   = "transfer"
+)
+
+var (
+	coin     string = Bitcoin
+	rawdests string = ""
+	action   string
+	wallet   crypto.Wallet
+
+	// transfer destination
+	dests       map[string]int64
+	dest_change string
+)
+
+func main() {
+	flag.StringVar(&coin, "coin", Bitcoin, strings.Join([]string{Bitcoin, Dogecoin, Ethereum}, ", "))
+	flag.StringVar(&rawdests, "dest", "", "Output destination (format=addr0:value0,addr1:value1,...)")
+	flag.StringVar(&action, "action", "", strings.Join([]string{GetAddress, GetBalance, Transfer}, ", "))
+	flag.Parse()
+
+	if len(action) == 0 {
+		flag.PrintDefaults()
+	}
+
+	// Open wallet
+	bank, err := microwallet.OpenBank(nil)
+	if err != nil {
+		log.Print(err)
+		log.Fatal("Unable to locate micro-wallet")
+	}
+	wallet, err = bank.Open(0)
+	if err != nil {
+		log.Print(err)
+		log.Fatal("Unable to open wallet")
+	}
+
+	// Parse destination
+	dests = make(map[string]int64)
+	for _, dest := range strings.Split(rawdests, ",") {
+		if len(dest) == 0 {
+			continue
+		}
+		addrvalue := strings.Split(dest, ":")
+		if len(addrvalue) != 2 {
+			log.Print(dest)
+			log.Fatal("wrong output dest format")
+		}
+		starget := addrvalue[0]
+		samount := addrvalue[1]
+		if samount == "*" {
+			dest_change = starget
+			continue
+		}
+		amount, err := strconv.ParseInt(samount, 10, 64)
+		if err != nil {
+			log.Print(err)
+			log.Fatal("Wrong wallet format")
+		}
+		dests[starget] = amount
+	}
+
+	// Coin decision
+	switch coin {
+	case Bitcoin:
+		coin_bitcoin()
+	case Dogecoin:
+		coin_dogecoin()
+	case Ethereum:
+		coin_ethereum()
+	default:
+		log.Fatal("unknown coin " + coin)
+	}
+}
