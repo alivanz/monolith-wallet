1ed7fd77f8a2a2114e34b271ba1fbfd6fb64a78c
alivan <alivan1627@gmail.com>
2018-10-31 02:51:03 +0700

    get address

--- monolith-wallet-cli/bitcoin.go
+++ monolith-wallet-cli/bitcoin.go
@@ -1,5 +1,26 @@
 package main
 
-func coin_bitcoin() {
+import (
+	"log"
 
+	"github.com/alivanz/go-crypto/bitcoin"
+)
+
+func coin_bitcoin() {
+	compressed := true
+	// get pubkey
+	pubkey, err := wallet.PubKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// get address
+	addr, err := bitcoin.PubKeyToAddress(bitcoin.MainNetworkDesc.PubKeyHashCode, bitcoin.SerializePubkey(pubkey, compressed))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch action {
+	case GetAddress:
+		log.Print(addr)
+	}
 }
